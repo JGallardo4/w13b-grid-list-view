d0efdc1d8a5ca980c9f3f86d7658a1674f625bcd
Juan Gallardo <juan.gallardo.ab@gmail.com>
2020-12-12 15:18:33 -0700

    Add dates to tweets

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,13 @@ export default {
 </script>
 
 <style>
-#app {
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+#app {	
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/components/tweets.vue
+++ src/components/tweets.vue
@@ -1,10 +1,14 @@
 <template>
 	<section id="tweets">
 		<button id="view-toggle-button" @click="toggleView()">Change View</button>
-		<section id="tweets-container" :class="this.listView ? 'list-view' : 'grid-view'">
+		<section
+			id="tweets-container"
+			:class="this.listView ? 'list-view' : 'grid-view'"
+		>
 			<article class="tweet" v-for="(tweet, index) in tweets" :key="index">
-				<h1>Author: {{ tweet.author }}</h1>
-				<p>{{ tweet.content }}</p>
+				<h1 class="tweet__author">Author: {{ tweet.author }}</h1>
+				<h2 class="tweet__date">Published on: {{ tweet.date }}</h2>
+				<p class="tweet__content">{{ tweet.content }}</p>
 			</article>
 		</section>
 	</section>
@@ -16,43 +20,79 @@ export default {
 
 	methods: {
 		toggleView() {
-		this.listView = !this.listView;
-		console.log(!this.listView);
-		}
+			this.listView = !this.listView;
+			console.log(!this.listView);
+		},
 	},
 
-  data() {
-    return {
+	data() {
+		return {
 			listView: false,
-      tweets: [
-        {
-          author: "Harry",
-          content: "I want the nimbus 3000",
-        },
-        {
-          author: "Ron",
-          content: "I hate spiders!",
-        },
-        {
-          author: "Hermione",
-          content: "You're gonna get us killed, or worse... expelled",
-        },
-      ],
-    };
-  },
+			tweets: [
+				{
+					author: "Harry",
+					content: "I want the nimbus 3000",
+					date: "October 10, 2001 ",
+				},
+				{
+					author: "Ron",
+					content: "I hate spiders!",
+					date: "October 20, 2001 ",
+				},
+				{
+					author: "Hermione",
+					content: "You're gonna get us killed, or worse... expelled",
+					date: "October 05, 2001 ",
+				},
+			],
+		};
+	},
 };
 </script>
 
 <style lang="scss" scoped>
-	#tweets-container {
+#tweets-container {
+	display: grid;
+	gap: 2rem;
+	padding: 2rem;
+
+	&.list-view {
+		grid-template-columns: 1fr;
+	}
+
+	&.grid-view {
+		grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+	}
+
+	.tweet {
 		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-auto-rows: auto 1fr;
+		padding: 1rem;
+		gap: 1rem;
+		background-color: lightgray;
+		border-radius: 10px;
 
-		&.list-view {
-			grid-template-columns: 1fr;
+		.tweet__author {
+			grid-column: 1;
+			grid-row: 1;
 		}
-
-		&.grid-view { 
-			grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+		.tweet__date {
+			grid-column: 2;
+			grid-row: 1;
+		}
+		.tweet__content {
+			grid-column: 1 / 3;
+			grid-row: 2;
 		}
 	}
+
+	h1 {
+		font-size: larger;
+	}
+
+	h2 {
+		font-size: large;
+	}
+}
 </style>>
